1eec9c101ed768eed31e0f55ff0708fe100785f9
影之 <1813058937@qq.com>
2023-05-24 23:11:41 +0800

    第一天

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,13 @@
     <van-button type="info">信息按钮</van-button>
     <van-button type="warning">警告按钮</van-button>
     <van-button type="danger">危险按钮</van-button>
+
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
+
+    <div class="box">Hello World</div>
   </div>
 </template>
 
@@ -21,4 +28,10 @@ export default {
 }
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+.box {
+  width: 555px;
+  height: 84px;
+  background-color: pink;
+}
+</style>
